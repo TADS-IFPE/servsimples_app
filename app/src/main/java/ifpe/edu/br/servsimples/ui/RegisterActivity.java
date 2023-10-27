45dd9b8983804719003551296afa2a377706b37b
Willian jefferson dos Santos <willjsan@motorola.com>
2023-10-27 15:41:20 -0300

    Cria tela de cadastro
    Cria tela com campos para cadastrar novo usuario

--- app/src/main/java/ifpe/edu/br/servsimples/ui/RegisterActivity.java
+++ app/src/main/java/ifpe/edu/br/servsimples/ui/RegisterActivity.java
@@ -1,14 +1,16 @@
-package ifpe.edu.br.servsimples;
+package ifpe.edu.br.servsimples.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ifpe.edu.br.servsimples.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
